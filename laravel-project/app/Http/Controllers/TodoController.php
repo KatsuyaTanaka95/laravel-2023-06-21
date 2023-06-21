7acfdeb594dc564313d73c0380226c95430175ae
KatsuyaTanaka95 <gu0520kt@gmail.com>
2023-06-21 17:29:32 +0900

    ユーザー認証機能

--- laravel-project/app/Http/Controllers/TodoController.php
+++ laravel-project/app/Http/Controllers/TodoController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Validator;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Todo;
+use Auth;
+
 
 class TodoController extends Controller
 {
+
+  public function __construct()
+  {
+    $this->middleware(['auth']);
+  }
+
   /**
    * Display a listing of the resource.
    *
